356653f3de2068bf25b7908df5e6dab03eb05010
ArnobMahmud <arnob.tech.me@gmail.com>
2020-09-23 10:00:38 +0600

    Accreditation MIT License

---
+++ OOP/class.js
@@ -0,0 +1,10 @@
+class MyClass {   // class created
+
+    myFun(){
+        console.log("Greetings from Arnob Mahmud.")
+    }
+}
+
+var object = new MyClass;  // object initialized
+
+object.myFun();   // function called by object
