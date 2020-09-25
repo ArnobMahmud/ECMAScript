a9901c96987b83bfb98a543572b90c6af7e6858b
ArnobMahmud <arnob.tech.me@gmail.com>
2020-09-25 21:20:52 +0600

    String in JS

---
+++ String/stringCompare.js
@@ -0,0 +1,4 @@
+var str1 = "abcde";
+var str2 = "cdeab"
+
+console.log(str1 == str2);
---
+++ String/stringLenght.js
@@ -0,0 +1,12 @@
+var name = "Arnob Mahmud";
+var lenght = 0;
+
+while (true) {
+    if (name.charAt(lenght) == '' )  {
+        break;
+    } else {
+        lenght++;
+    }
+}
+
+console.log(lenght);
