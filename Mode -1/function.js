fc4d01ae2541d70c85b3e795c6d7056dbd2e1f47
ArnobMahmud <arnob.tech.me@gmail.com>
2020-09-25 09:15:12 +0600

    Another Function Added

--- Mode -1/function.js
+++ Mode -1/function.js
@@ -1,11 +1,22 @@
 function addNumber(...numbers) {
-    let sum = 0;
+    let sum1 = 0;
 
-    for(let i of numbers){
-        sum += i;
+    for (let i of numbers) {
+        sum1 += i;
     }
-        
-    console.log(sum);
+
+    console.log(sum1);
 }
 
-addNumber(1, 3, 6, 8, 45, 34, 35 ,33, 73, 93);
+addNumber(1, 3, 6, 8, 45, 34, 35, 33, 73, 93);
+
+function multiPlyNum(...numbers) {
+    let sum2 = 0;
+
+    for (const j of numbers) {
+        sum += j;
+    }
+
+    console.log(sum2);
+
+}
