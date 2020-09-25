da23fcc420323960ec0a5be89190cbd8128f819d
ArnobMahmud <arnob.tech.me@gmail.com>
2020-09-25 20:14:12 +0600

    Condition in JS

---
+++ Conditions/condition-shortcut.js
@@ -0,0 +1,5 @@
+var a = 4;
+var b = 7;
+
+var bigNum = a ? b : b;
+console.log(bigNum);
---
+++ Conditions/max-num.js
@@ -0,0 +1,11 @@
+var a = 5;
+var b = 6;
+var c = 9;
+
+if (a > b && a > c) {
+    console.log(a + " is the biggest number ");
+} else if(b > a && b > c) {
+    console.log(b + " is the biggest number ");
+}else {
+    console.log(c + " is the biggest number ");
+}
