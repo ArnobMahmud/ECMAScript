819df4fa56dc8baa0f3a0f891531a73f9f238c07
ArnobMahmud <arnob.tech.me@gmail.com>
2020-12-22 18:34:51 +0600

    update

---
+++ ES6/let vs var vs const/let2.js
@@ -0,0 +1,10 @@
+let x = "Hello World";
+var a = 5;
+
+// if (a == 5) {
+//     let x = "Hello Mars";
+//     console.log(x);        // will print Hello Mars
+// }
+
+var x = "Hello Mars";
+console.log(x);          // will print Hello World
